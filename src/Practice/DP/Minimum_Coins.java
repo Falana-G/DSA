819ebe724aa0c41da4bfc76e530ebf34e00677e3
Falana-G <keeratchawla56@gmail.com>
2026-06-10 18:32:45 +0530

    Min Coins Question

---
+++ src/Practice/DP/Minimum_Coins.java
@@ -0,0 +1,29 @@
+package Practice.DP;
+
+/*
+A chocolate shop gives special reward coins.
+Available coin values:
+1
+4
+5
+A customer wants to redeem exactly 8 chocolates.
+Find the minimum number of coins required.
+A coin may be used multiple times.
+Input
+3
+1 4 5
+8
+Output
+2
+Explanation
+Use:
+4 + 4
+Only 2 coins.
+Using:
+5 + 1 + 1 + 1
+requires 4 coins.
+*/
+
+
+public class Minimum_Coins {
+}
