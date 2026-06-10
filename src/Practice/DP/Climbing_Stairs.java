410bd0246e70408e4dc3d2ec89aebc457c487cb3
Falana-G <keeratchawla56@gmail.com>
2026-06-10 18:02:05 +0530

    Practice folder

---
+++ src/Practice/DP/Climbing_Stairs.java
@@ -0,0 +1,33 @@
+package Practice.DP;
+
+/*
+A research company has developed a robot that needs to travel through a vertical energy tunnel.
+The tunnel contains N energy pods arranged one above another.
+The robot starts at pod 0 and wants to reach pod N.
+Due to its design limitations, the robot can:
+Jump to the next pod (+1)
+Jump over one pod (+2)
+Find the total number of distinct ways the robot can reach pod N.
+Since the number of ways can be large, print the answer.
+
+Input
+4
+Output
+5
+Explanation:
+The possible paths are:
+1 1 1 1
+1 1 2
+1 2 1
+2 1 1
+2 2
+Total ways:
+5
+*/
+
+
+public class Climbing_Stairs {
+    static void main(String[] args) {
+
+    }
+}
