4013fe24c674c9c61424eb133e08cb1125295028
Falana-G <keeratchawla56@gmail.com>
2026-06-11 12:04:27 +0530

    String Reverse Recursion

--- src/Recursion/String_Reverse.java
+++ src/Recursion/String_Reverse.java
@@ -1,4 +1,15 @@
 package Recursion;
 
 public class String_Reverse {
+    static void main(String[] args) {
+        String str = "ABCDEF";
+        System.out.println(rev(str, str.length()-1));
+    }
+    public static String rev(String str, int i){
+        if(i<0){
+            return "";
+        }
+        char ch = str.charAt(i);
+        return ch+rev(str, i-1);
+    }
 }
--- src/Recursion/Sum_of_Elements_of_Array.java
+++ src/Recursion/Sum_of_Elements_of_Array.java
@@ -1,4 +1,17 @@
 package Recursion;
 
 public class Sum_of_Elements_of_Array {
+    static void main(String[] args) {
+        int []arr = {1,3,2,5};
+        int sum = Sum(arr, arr.length-1);
+        System.out.println(sum);
+    }
+
+    public static int Sum(int []arr, int length){
+        if(length >= 0){
+            return arr[length] + Sum(arr, length-1);
+        }else{
+            return 0;
+        }
+    }
 }
