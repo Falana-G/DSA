7d4fe684e79893b1503279e4fa8eb923d5da4c35
Falana-G <keeratchawla56@gmail.com>
2026-06-11 12:33:08 +0530

    String Reverse Tail&Head

--- src/Recursion/String_Reverse.java
+++ src/Recursion/String_Reverse.java
@@ -3,13 +3,24 @@ package Recursion;
 public class String_Reverse {
     static void main(String[] args) {
         String str = "ABCDEF";
-        System.out.println(rev(str, str.length()-1));
+        String ans = "";
+        System.out.println(Headrev(str, str.length()-1));
+        Tailrev(str, str.length()-1, ans);
     }
-    public static String rev(String str, int i){
+    public static String Headrev(String str, int i){
         if(i<0){
             return "";
         }
         char ch = str.charAt(i);
-        return ch+rev(str, i-1);
+        return ch+Headrev(str, i-1);
+    }
+    public static void Tailrev(String str, int i, String ans){
+        if(i<0){
+            System.out.println(ans);
+        }
+        else{
+            ans = ans + str.charAt(i);
+            Tailrev(str, i-1, ans);
+        }
     }
 }
