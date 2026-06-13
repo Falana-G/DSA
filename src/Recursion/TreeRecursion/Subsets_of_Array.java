d9c83e6e89cc6e320777ba93dbb09c3b8bbb9d6e
Falana-G <keeratchawla56@gmail.com>
2026-06-13 14:56:19 +0530

    Subsets of Array Recursion

---
+++ src/Recursion/TreeRecursion/Subsets_of_Array.java
@@ -0,0 +1,45 @@
+package Recursion.TreeRecursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Subsets_of_Array {
+    static void main(String[] args) {
+        int []arr = {1,2,3};
+        String ans = "";
+        System.out.println(subsets(arr));
+    }
+
+    static public ArrayList<ArrayList<Integer>> subsets(int []arr){
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+        subset(arr, 0, "", mainlist);
+        return mainlist;
+    }
+    static void subset(int[] arr, int i, String ans, ArrayList<ArrayList<Integer>> mainlist) {
+        if (i == arr.length) {
+            ans = ans.trim();
+            ArrayList<Integer> al = convert(ans);
+            mainlist.add(al);
+        }
+        else{
+            // Include
+            String temp = ans + arr[i] + " ";
+            subset(arr, i + 1, temp, mainlist);
+
+            //Don't Include
+            subset(arr, i + 1, ans, mainlist);
+        }
+    }
+
+    static ArrayList<Integer> convert(String str){
+        ArrayList<Integer> al = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(str);
+        int n = st.countTokens();
+        while(st.hasMoreTokens()){
+            int a = Integer.parseInt(st.nextToken());
+            al.add(a);
+        }
+        return al;
+    }
+}
