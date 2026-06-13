1086a21ebf0be0fce5ec5df589401af1907e6247
Falana-G <keeratchawla56@gmail.com>
2026-06-13 15:22:06 +0530

    Added Count nodes in Subsets of String

--- src/Recursion/TreeRecursion/Subsets_of_String.java
+++ src/Recursion/TreeRecursion/Subsets_of_String.java
@@ -4,20 +4,25 @@ public class Subsets_of_String {
     static void main(String[] args) {
         String str = "ABC";
         String ans = "";
-        subset(str, 0, ans);
+        int []count = {0, 0};
+        subset(str, 0, ans, count);
+        System.out.println("\nNo. of Base condition nodes: " + count[0]);
+        System.out.println("No. of nodes: " + count[1]);
     }
-    static void subset(String str, int i, String ans){
+    static void subset(String str, int i, String ans, int[] count){
+
+        count[1]++;
         if(i == str.length()){
             System.out.print(ans + ", ");
+            count[0]++;
         }
         else{
-            char ch = str. charAt(i);
-
             // Include
-            subset(str, i+1, ans+ch);
+            char ch = str. charAt(i);
+            subset(str, i+1, ans+ch, count);
 
             // Don't Include
-            subset(str, i+1, ans);
+            subset(str, i+1, ans, count);
         }
     }
 
